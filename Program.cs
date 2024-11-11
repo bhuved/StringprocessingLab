02f18f7217a3b24a64f644c114f76303522cb088
bhuved <bhuvedu@gmail.com>
2024-11-11 12:39:55 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***Basic String processing Lab");
+do
+{
+    Console.WriteLine("Enter Some sentence");
+    string str = Console.ReadLine();
+    Console.WriteLine(str);
+    string[] words = str.Split(' ');
+    foreach (string word in words)
+    {
+        Console.WriteLine(word);
+    }
+
+} while (GetPlayAgainAnswer() == true);
+
+//Console.ReadKey();
+
+bool GetPlayAgainAnswer(string strMessage = "Would you like to Continue? y/n")
+{
+    Console.WriteLine(strMessage);
+    string userAnswer = Console.ReadLine();
+    if (userAnswer.ToLower() != "y")
+    {
+        return false;
+    }
+
+    //Console.WriteLine("YEAH LETS PLAY");
+    return true;
+}
+
+List<string> addTextInList = new List<string>();
+do
+{
+    Console.WriteLine("Enter Some Text");
+    string str = Console.ReadLine().Trim();
+    Console.WriteLine("You have entered "+str);
+    addTextInList.Add(str);
+   
+} while (GetPlayAgainAnswer() == true);
+
+string concantenateText = "";
+foreach (string word in addTextInList)
+{
+    concantenateText += word + " ";
+}
+Console.WriteLine(concantenateText);
+
+
